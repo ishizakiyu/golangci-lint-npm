ed58f5ff7c4020718dde86f22d3b43039a96053b
Yu Ishizaki <yizk.dev@gmail.com>
2025-05-10 21:22:07 +0900

    chore: set up development tools

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"],
+        },
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
